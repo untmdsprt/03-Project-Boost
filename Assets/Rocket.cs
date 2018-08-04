73b050fabe0cf56166417ba430d6710f4ed42879
Jenny <untmdsprt@gmail.com>
2018-08-04 16:29:55 -0500

    S3L56 - Invoke() As A Coroutine Warm-up

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
@@ -11,69 +9,72 @@ public class Rocket : MonoBehaviour {
 	Rigidbody rigidBody;
     AudioSource MyAudioSource;
 
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
 
 	// Use this for initialization
-	void Start () 
-    {
+	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
         MyAudioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
-	void Update () 
-    {
-        Thrust();
-        Rotate();
+	void Update () {
+        if (state == State.Alive) {
+            Thrust();
+            Rotate();
+        }
 	}
 
-    void OnCollisionEnter(Collision collision) 
-    {
-        switch (collision.gameObject.tag) 
-        {
+    void OnCollisionEnter(Collision collision) {
+        if (state != State.Alive) { return; } // ignore collisions when dead
+        switch (collision.gameObject.tag) {
             case "Friendly":
                 // do nothing
-                print("OK"); // todo remove
                 break;
-
-            case "Dead":
-                print("Boom!");
-                // kill player
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1f); // parameterize time
+                break;
+            default:
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1f);
                 break;
         }
     }
-    private void Thrust()
-    {
-        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
-        {
+
+    private void LoadNextLevel() {
+        SceneManager.LoadScene(1); // TODO allow for more than 2 levels
+    }
+
+    private void LoadFirstLevel() {
+        SceneManager.LoadScene(0);
+    }
+
+    private void Thrust() {
+        if (Input.GetKey(KeyCode.Space)) {  // can thrust while rotating
             rigidBody.AddRelativeForce(Vector3.up * mainThrust);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        if (Input.GetKeyDown(KeyCode.Space)) {
             MyAudioSource.Play();
         }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
+        if (Input.GetKeyUp(KeyCode.Space)) {
             MyAudioSource.Stop();
         }
     }
 
-    private void Rotate()
-    {
+    private void Rotate() {
         rigidBody.freezeRotation = true; // take manual control of rotation
 
         float rotationThisFrame = rcsThrust * Time.deltaTime;
-        if (Input.GetKey(KeyCode.A))
-        {
+        if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(Vector3.forward * rotationThisFrame);
         }
 
-        else if (Input.GetKey(KeyCode.D))
-        {
+        else if (Input.GetKey(KeyCode.D)) {
             transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false; // resume physics control of rotation
     }
-
-
 }
