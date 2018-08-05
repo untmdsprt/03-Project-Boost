ffaa238cf7cafbe9d9153ff334f1597160db7ae1
Jenny <untmdsprt@gmail.com>
2018-08-04 23:19:23 -0500

    S3L60 - Moving Platform Pattern

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector;
+
+	// TODO remove from inspector later
+	// 0 for not moved, 1 for fully moved.
+	[Range(0,1)][SerializeField]float movementFactor;
+
+	Vector3 startingPos;
+
+	// Use this for initialization
+	void Start () {
+		startingPos = transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 offset = movementVector * movementFactor;
+		transform.position = startingPos + offset;
+	}
+}
